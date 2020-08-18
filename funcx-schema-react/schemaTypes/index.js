a515235d9453d6cb393a47d443e974eb32c372cc
norami <norami.dream@gmail.com>
2020-08-18 12:33:41 +0900

    babel 7 + es5 trasnform problem

--- funcx-schema-react/schemaTypes/index.js
+++ funcx-schema-react/schemaTypes/index.js
@@ -1,18 +1,44 @@
-const importAll = (context) => {
-  const modules = {};
-  for (const key of context.keys()) {
-    const match = key.match(/\.\/([a-zA-Z]+)\.js$/);
-    if (match) {
-      const name = match[1];
-      if (name !== "index") {
-        modules[name] = context(key);
-      }
-    }
-  }
-  return modules;
+import * as actionButton from "./actionButton";
+import * as array from "./array";
+import * as audio from "./audio";
+import * as audioMulti from "./audioMulti";
+import * as boolean from "./boolean";
+import * as date from "./date";
+import * as datetime from "./datetime";
+import * as dialog from "./dialog";
+import * as dynamic from "./dynamic";
+import * as email from "./email";
+import * as file from "./file";
+import * as image from "./image";
+import * as multiType from "./multiType";
+import * as number from "./number";
+import * as object from "./object";
+import * as password from "./password";
+import * as selector from "./selector";
+import * as selectorMulti from "./selectorMulti";
+import * as string from "./string";
+import * as stringMultiline from "./stringMultiline";
+import * as text from "./text";
+export {
+  actionButton,
+  array,
+  audio,
+  audioMulti,
+  boolean,
+  date,
+  datetime,
+  dialog,
+  dynamic,
+  email,
+  file,
+  image,
+  multiType,
+  number,
+  object,
+  password,
+  selector,
+  selectorMulti,
+  string,
+  stringMultiline,
+  text,
 };
-/* global require */
-const webPackContext = require.context("./", true, /^.\/([a-zA-Z]+)\.js$/);
-const modules = importAll(webPackContext);
-
-module.exports = modules;
